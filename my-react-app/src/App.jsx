236dd87a085b431911abf4f794d791a63912e54e
اسمك هنا < >
2025-10-24 13:20:07 +0300

    ///kdmdoj

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,22 +3,12 @@ import './App.css';
 import Header from './Header';
 import Post from './Post';
 // import SideBar from './SideBar';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <div className={"main-layout"}>
-          <div className={"posts"}>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-          </div>
-          {/* <SideBar /> */}
-        </div>
+        {/* <SideBar /> */}
       </div>
     );
   }
