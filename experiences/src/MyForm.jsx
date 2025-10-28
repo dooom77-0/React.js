0b70d51e92fa4eb11460df71797c650f2eee54a2
اسمك هنا < >
2025-10-28 08:30:32 +0300

    ,,

--- experiences/src/MyForm.jsx
+++ experiences/src/MyForm.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
 export default function MyForm() {
-    const [FormValue, setFormValue] = useState({ name: '', email: '' })
-    console.log(FormValue);
+    const [FormValue, setFormValue] = useState({ name: '', email: '', age: '', generalInfo: '', isStudent: false })
     return (
         <form style={{ padding: '20px', margin: '20px', border: '5px solid teal', borderRadius: '10px', color: '#fff', textAlign: 'center' }} onSubmit={(e) => { e.preventDefault();}}>
             <label>Name :</label>
-            <input value={FormValue.name} onChange={(e) => {setFormValue({name : e.target.value, email : FormValue.email})}} />
+            <input value={FormValue.name} onChange={(e) => {setFormValue({...FormValue, name : e.target.value})}} />
             <hr/>
             <label>email :</label>
-            <input value={FormValue.email} onChange={(e) => setFormValue({email: e.target.value, name : FormValue.name })} />
+            <input value={FormValue.email} onChange={(e) => setFormValue({...FormValue, email : e.target.value})} />
+            <hr />
+            <label>Age :</label>
+            <input value={FormValue.age} onChange={(e) => setFormValue({...FormValue, age : e.target.value})} />
+            <hr />
+            <label>Are you a student :</label>
+            <input type='checkbox' checked={FormValue.isStudent} onChange={(e) => setFormValue({...FormValue, isStudent : e.target.checked})} />
             <hr/>
-            <button>Submit</button>
+            <label>General Info :</label>
+            <textarea value={FormValue.generalInfo} onChange={(e) => setFormValue({...FormValue, generalInfo : e.target.value})} />
+            <hr />
             
+            <button>Submit</button>            
         </form>
     )
 }
